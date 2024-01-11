5e1e44cc32d375bc48faa0b4795c8ccde8f26e6e
siukatech <siuka114@gmail.com>
2024-01-11 21:03:01 +0800

    add SxProps to IconComponent

--- src/frameworks/ui/components/IconComponent.tsx
+++ src/frameworks/ui/components/IconComponent.tsx
@@ -1,23 +1,24 @@
 import * as Icons from '@mui/icons-material';
+import { SxProps } from '@mui/material';
 
 type IconNames = keyof typeof Icons;
 type IconProps = {
   name: IconNames;
+  sx?: SxProps;
 };
-
 
 /**
  * References:
  * https://stackoverflow.com/a/75906013
- * 
- * @param param0 
- * @returns 
+ *
+ * @param param0
+ * @returns
  */
-const IconComponent: React.FC<IconProps> = ({ name }) => {
+const IconComponent: React.FC<IconProps> = ({ name, sx }) => {
   const Icon = Icons[name];
   return (
     <>
-      <Icon />
+      <Icon sx={sx} />
     </>
   );
 };
@@ -25,4 +26,3 @@ const IconComponent: React.FC<IconProps> = ({ name }) => {
 export default IconComponent;
 
 export type { IconNames, IconProps };
-
