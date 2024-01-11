9610d88ac8295e09840b492edb28b2c6724f498a
siukatech <siuka114@gmail.com>
2024-01-11 21:04:06 +0800

    add Attachment feature

---
+++ src/frameworks/attachment/components/AttachmentPane.tsx
@@ -0,0 +1,258 @@
+import { Fragment, useEffect, useRef, useState } from 'react';
+
+import { useTranslation } from 'react-i18next';
+import { IAttachmentObj } from '../models';
+import { styled } from '@mui/material/styles';
+import {
+  deleteAttachmentObj,
+  downloadAttachmentObj,
+  uploadAttachmentObjList,
+} from '../services/AttachmentService';
+import {
+  Box,
+  Button,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  Typography,
+} from '@mui/material';
+import { IconComponent } from '../../ui';
+import { el } from 'date-fns/locale';
+
+const MAX_RETRY = 5;
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+type AttachmentPaneProps = {
+  readOnly?: boolean;
+  attachmentObjList?: IAttachmentObj[];
+  onAttachmentListChange: (
+    attachmentObjList: IAttachmentObj[],
+    isUploading: boolean
+  ) => void;
+};
+
+const AttachmentPane: React.FC<AttachmentPaneProps> = ({
+  readOnly,
+  attachmentObjList,
+  onAttachmentListChange,
+}) => {
+  const { t } = useTranslation();
+
+  const fileInputRef = useRef<HTMLInputElement>(null); // null is required for the VisuallyHiddenInput's ref
+  const [valueList, setValueList] = useState<IAttachmentObj[]>(() => {
+    let list: IAttachmentObj[] = [];
+    list = attachmentObjList
+      ? attachmentObjList.map((attachmentObj, idx) => {
+          return {
+            ...attachmentObj,
+            isUploaded:
+              attachmentObj.isUploaded != null
+                ? attachmentObj.isUploaded
+                : true,
+          } as IAttachmentObj;
+        })
+      : [];
+    return list;
+  });
+  const [isUploading, setIsUploading] = useState(false);
+  const [isReadOnly, setIsReadOnly] = useState(() => {
+    return readOnly != null ? readOnly : false;
+  });
+  const [valueErr, setValueErr] = useState<any>();
+  const [retryCount, setRetryCount] = useState(0);
+
+  useEffect(() => {
+    let filteredList = valueList.filter(
+      (attachmentObj) => attachmentObj.isUploaded === false
+    );
+    if (filteredList.length > 0 && retryCount < MAX_RETRY) {
+      // upload
+      const uploadAttachment = async () => {
+        setRetryCount((prevState) => prevState + 1);
+        const uploadedList = await uploadAttachmentObjList(valueList);
+        setValueList(uploadedList);
+        setIsUploading(false);
+        if (fileInputRef.current && fileInputRef.current.value) {
+          fileInputRef.current.value = '';
+        }
+      };
+      uploadAttachment();
+    } else {
+      setIsUploading(false);
+    }
+    console.debug(
+      `AttachmentPane - useEffect - filteredList.length: [${filteredList.length}], valueList: `,
+      valueList
+    );
+    onAttachmentListChange(valueList, filteredList.length > 0);
+  }, [valueList, isUploading]);
+
+  const handleAttachmentChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const targetFiles = evt.target.files ? evt.target.files : [];
+    console.debug(
+      `AttachmentPane - handleAttachmentChange - targetFiles: `,
+      targetFiles
+    );
+    setValueList((prevState) => {
+      let valueList = prevState;
+      const valueMap: any = valueList
+        ? valueList.reduce(
+            (accumulator, attachmentObj, idx) => ({
+              ...accumulator,
+              [attachmentObj.fileName]: attachmentObj.fileName,
+            }),
+            {}
+          )
+        : {};
+      console.debug(
+        `AttachmentPane - handleAttachmentChange - valueMap: `,
+        valueMap
+      );
+      for (let ccc = 0; ccc < targetFiles.length; ccc++) {
+        const targetFile = targetFiles[ccc];
+        const valueMapTargetFile = valueMap[targetFile.name];
+        console.debug(
+          `AttachmentPane - handleAttachmentChange - for - valueMapTargetFile is null: [${
+            valueMapTargetFile == null
+          }], targetFile: `,
+          targetFile
+        );
+        if (targetFile && valueMap[targetFile.name] == null) {
+          valueList.push({
+            fileName: targetFile.name,
+            fileSize: targetFile.length,
+            id: undefined,
+            isUploaded: false,
+            targetFile,
+          } as IAttachmentObj);
+        }
+      }
+      return valueList;
+    });
+    setIsUploading(true);
+  };
+
+  const handleAttachmentDownload = (
+    attachmentObj: IAttachmentObj,
+    idx: number
+  ) => {
+    const downloadFile = async (attachmentObj: IAttachmentObj) => {
+      try {
+        await downloadAttachmentObj(attachmentObj);
+      } catch (err) {
+        setValueErr(err);
+      }
+    };
+    if (attachmentObj.isUploaded && attachmentObj.uploadErr == null) {
+      downloadFile(attachmentObj);
+    }
+  };
+
+  const handleAttachmentDelete = (
+    attachmentObj: IAttachmentObj,
+    idx: number
+  ) => {
+    const attachmentObjToDelete = valueList.at(idx);
+    const clearFile = (idx: number) => {
+      setValueList((prevState) => {
+        let valueListPrev = prevState;
+        valueListPrev.splice(idx, 1);
+        let valueListNew: IAttachmentObj[] = [];
+        for (let ccc = 0; ccc < valueListPrev.length; ccc++) {
+          valueListNew.push(valueListPrev[ccc]);
+        }
+        return valueListNew;
+      });
+    };
+    console.debug(
+      `AttachmentPane - handleAttachmentDelete - idx: [${idx}], attachmentObj: `,
+      attachmentObj
+    );
+    if (attachmentObj.isUploaded && !attachmentObj.uploadErr) {
+      const deleteFile = async (attachmentObj: IAttachmentObj) => {
+        const { data } = await deleteAttachmentObj(attachmentObj);
+        return data;
+      };
+      deleteFile(attachmentObj).then((retData) => clearFile(idx));
+    } else {
+      clearFile(idx);
+    }
+  };
+
+  return (
+    <>
+      <Box>
+        <Typography variant="body1">{t('upload.title')}</Typography>
+      </Box>
+      <List>
+        {valueList.map((attachmentObj: IAttachmentObj, idx: number) => {
+          console.debug(
+            `AttachmentPane - return - idx: [${idx}], attachmentObj: `,
+            attachmentObj
+          );
+          return (
+            <Fragment key={`key-attachment-${idx}`}>
+              <ListItem sx={{ pl: 0 }}>
+                {attachmentObj.isUploaded && (
+                  <Link
+                    sx={{ cursor: 'pointer' }}
+                    onClick={() => handleAttachmentDownload(attachmentObj, idx)}
+                  >
+                    {attachmentObj.fileName}
+                  </Link>
+                )}
+                {!attachmentObj.isUploaded && (
+                  <Typography variant="body1">
+                    {attachmentObj.fileName}...
+                  </Typography>
+                )}
+                {!isReadOnly &&
+                  attachmentObj.isUploaded && (
+                    <ListItemIcon
+                      sx={{ minWidth: 'auto', pl: 0.5, cursor: 'pointer' }}
+                      onClick={() => handleAttachmentDelete(attachmentObj, idx)}
+                    >
+                      <IconComponent name="DeleteOutline" />
+                    </ListItemIcon>
+                  )}
+                {!isReadOnly &&
+                  attachmentObj.isUploaded &&
+                  attachmentObj.uploadErr && (
+                    <ListItemIcon
+                      sx={{ minWidth: 'auto', pl: 0.5, cursor: 'pointer' }}
+                      title={attachmentObj.uploadErr}
+                    >
+                      <IconComponent name="ErrorOutline" />
+                    </ListItemIcon>
+                  )}
+              </ListItem>
+            </Fragment>
+          );
+        })}
+      </List>
+      <Button component="label" disabled={isUploading || isReadOnly}>
+        <IconComponent name="UploadFile" sx={{ pr: 0.5 }} />
+        <Typography variant="body1">{t('button.upload')}</Typography>
+        <VisuallyHiddenInput
+          type="file"
+          onChange={handleAttachmentChange}
+          ref={fileInputRef}
+        />
+      </Button>
+    </>
+  );
+};
+
+export default AttachmentPane;
---
+++ src/frameworks/attachment/services/AttachmentService.tsx
@@ -0,0 +1,240 @@
+import { uuidv4 } from 'uuidv7';
+import { AxiosError, AxiosProgressEvent } from 'axios';
+import axiosService from '../../../frameworks/axios/services/axios-service';
+import { IAttachmentObj } from '../models';
+import { at, bind } from 'lodash';
+
+const API_DOMAIN: string = process.env.REACT_APP_API_PATH_PREFIX as string;
+const API_UPLOAD: string =
+  (API_DOMAIN as string) +
+  (process.env.REACT_APP_API_PATH_V1_PROTECTED as string) +
+  (process.env.REACT_APP_API_PATH_ATTACHMENT_UPLOAD as string) +
+  '';
+const API_DETAIL: string =
+  (API_DOMAIN as string) +
+  (process.env.REACT_APP_API_PATH_V1_PROTECTED as string) +
+  (process.env.REACT_APP_API_PATH_ATTACHMENT_DETAIL as string) +
+  '';
+const API_DOWNLOAD: string =
+  (API_DOMAIN as string) +
+  (process.env.REACT_APP_API_PATH_V1_PROTECTED as string) +
+  (process.env.REACT_APP_API_PATH_ATTACHMENT_DOWNLOAD as string) +
+  '';
+const API_DELETE: string =
+  (API_DOMAIN as string) +
+  (process.env.REACT_APP_API_PATH_V1_PROTECTED as string) +
+  (process.env.REACT_APP_API_PATH_ATTACHMENT_DELETE as string) +
+  '';
+
+let FILE_UPLOAD_MAP: any = {};
+
+const uploadAttachmentObj = async (
+  formData: FormData,
+  total: number,
+  pending: number
+): Promise<any> => {
+  const handleUploadProgress = (evt: AxiosProgressEvent) => {
+    const percentage = Math.round(100 / evt.loaded / (evt?.total || 1));
+    console.debug(
+      `AttachmentService - uploadAttachmentObj - handleUploadProgress - pencentage: [${percentage}]`
+    );
+  };
+  try {
+    const response = await axiosService.post(`${API_UPLOAD}`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        // mode: 'cors',
+      },
+      onUploadProgress: handleUploadProgress,
+    });
+    if (response instanceof AxiosError) {
+      const err = response;
+      if (err.response?.data) {
+        throw new Error(JSON.stringify(err.response?.data));
+      } else throw response;
+    }
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+/**
+ * Reference:
+ * https://www.bezkoder.com/axios-file-upload/
+ * https://youtu.be/YOGgaYUW1OA?t=320
+ *
+ * @param attachmentObjList
+ * @returns
+ */
+const uploadAttachmentObjList = async (attachmentObjList: IAttachmentObj[]) => {
+  const readyToUploadList = attachmentObjList.filter(
+    (uploadAttachmentObj) =>
+      uploadAttachmentObj.targetFile != null && !uploadAttachmentObj.isUploaded
+  );
+  const total = readyToUploadList.length;
+  let pending = total;
+  const uploadedList: IAttachmentObj[] = [];
+  for (let ccc = 0; ccc < readyToUploadList.length; ccc++) {
+    const attachmentObj = attachmentObjList[ccc];
+    if (attachmentObj.targetFile && !attachmentObj.isUploaded) {
+      let formData: FormData = new FormData();
+      formData.append('file', attachmentObj.targetFile);
+      formData.append('versionNo', '1');
+      try {
+        const retData = await uploadAttachmentObj(formData, total, pending);
+        if (!retData) {
+          throw new Error(`Upload attachment failed`);
+        }
+        pending--;
+        attachmentObj.id = retData.id;
+        if (attachmentObj.id) {
+          FILE_UPLOAD_MAP[attachmentObj.id.toString()] = attachmentObj;
+          // const arrBuf = new Blob([await attachmentObj.targetFile.arrayBuffer()]);
+          // FILE_UPLOAD_MAP[attachmentObj.id.toString()] = arrBuf;
+        }
+        attachmentObj.isUploaded = true;
+        attachmentObj.targetFile = undefined;
+        uploadedList.push(attachmentObj);
+      } catch (err) {
+        console.error(
+          `AttachmentService - uploadAttachmentObjList - err: `,
+          err
+        );
+        pending--;
+        attachmentObj.uploadErr = err;
+        attachmentObj.isUploaded = true;
+        attachmentObj.targetFile = undefined;
+        uploadedList.push(attachmentObj);
+      }
+    } else if (attachmentObj.isUploaded) {
+      uploadedList.push(attachmentObj);
+    }
+  }
+  return uploadedList;
+};
+
+const getAttachmentObj = async (id: string): Promise<IAttachmentObj> => {
+  const apiUrl = API_DETAIL.replaceAll('{0}', id);
+  const { data } = await axiosService.get(`${apiUrl}`);
+  return data as IAttachmentObj;
+};
+
+/**
+ * Reference:
+ * https://stackoverflow.com/a/31218143
+ * https://stackoverflow.com/a/63965930
+ * https://gist.github.com/jbutko/d7b992086634a94e84b6a3e526336da3
+ *
+ * @param attachmentObj
+ * @returns
+ */
+const downloadAttachmentObj = async (
+  attachmentObj: IAttachmentObj
+): Promise<any> => {
+  if (attachmentObj.id) {
+    const doDownload = (
+      byteArr: ArrayBuffer | Blob,
+      attachmentObj: IAttachmentObj
+    ) => {
+      const blob: Blob = new Blob([byteArr], {
+        type: attachmentObj.contentType,
+      });
+      const a = document.createElement('a');
+
+      const url = URL.createObjectURL(blob);
+      console.debug(
+        `AttachmentService - downloadAttachmentObj - doDownload - url: [${url}]`
+      );
+      function handleClick() {
+        setTimeout(() => {
+          URL.revokeObjectURL(url);
+          /* eslint-disable-next-line no-restricted-globals */
+          removeEventListener('click', handleClick);
+        }, 150);
+      }
+      a.href = url;
+      a.download = attachmentObj.fileName || 'download-file';
+      a.addEventListener('click', handleClick);
+      a.click();
+
+      // let reader = new FileReader();
+      // reader.readAsDataURL(blob); // converts the blob to base64 and calls onload
+      // reader.onload = function () {
+      //   if (typeof reader.result === 'string') {
+      //     a.href = reader.result; // data url
+      //     a.click();
+      //   }
+      // };
+    };
+    const apiUrl = API_DOWNLOAD.replaceAll('{0}', attachmentObj.id.toString());
+    const response = await axiosService.get(`${apiUrl}`, {
+      // headers: {
+      //   'Content-Type': 'application/json',
+      //   Accept: attachmentObj.contentType,
+      // },
+      responseType: 'arraybuffer',
+      // responseType: 'blob',
+    });
+    if (response?.data) {
+      console.debug(
+        `AttachmentService - downloadAttachmentObj - response.data.length: [${response.data.length}], response.data: `,
+        response.data
+      );
+      doDownload(response.data, attachmentObj);
+    } else {
+      throw new Error(
+        `Attachment not found [${attachmentObj?.id?.toString()}]`
+      );
+    }
+    return void 0;
+  } else {
+    return Promise.reject(attachmentObj);
+  }
+};
+
+const binaryStrToArrayBuffer = (binaryStr: string) => {
+  let byteArr = null;
+  // let binaryLen = binaryStr.length;
+  // byteArr = new Uint8Array(binaryLen);
+  // for (let ccc = 0; ccc < binaryLen; ccc++) {
+  //   let ascii = binaryStr.charCodeAt(ccc);
+  //   byteArr[ccc] = ascii;
+  // }
+  byteArr = Uint8Array.from(binaryStr, (c) => c.charCodeAt(0));
+  return byteArr;
+};
+
+const base64ToArrayBuffer = (base64Str: string) => {
+  let byteArr = null;
+  let binaryStr = atob(base64Str);
+  byteArr = binaryStrToArrayBuffer(binaryStr);
+  return byteArr;
+};
+
+const entityEncode = (arr: Uint8Array) => {
+  return Array.from(arr).map((val) => 'U+' + toHex(val));
+};
+const toHex = (num: number) => {
+  return num.toString(16).padStart(4, '0').toUpperCase();
+};
+
+const deleteAttachmentObj = async (
+  attachmentObj: IAttachmentObj
+): Promise<any> => {
+  if (attachmentObj.id) {
+    const apiUrl = API_DELETE.replaceAll('{0}', attachmentObj.id?.toString());
+    const response = await axiosService.delete(`${apiUrl}`);
+    return response;
+  } else {
+    Promise.reject(attachmentObj);
+  }
+};
+
+export {
+  uploadAttachmentObj,
+  uploadAttachmentObjList,
+  getAttachmentObj,
+  downloadAttachmentObj,
+  deleteAttachmentObj,
+};
